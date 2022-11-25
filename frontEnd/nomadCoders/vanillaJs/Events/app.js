43e86b29ef6e26e8281b6a15db5ecbd7e2c43574
MS <2206017@naver.com>
2022-11-25 18:43:01 +0900

    end JSevent

--- frontEnd/nomadCoders/vanillaJs/Events/app.js
+++ frontEnd/nomadCoders/vanillaJs/Events/app.js
@@ -1,11 +1,7 @@
-// html태그에 접근
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-// title div를 클릭시 발생할 function 정의
-function handleTitleClick () {
-    console.log(title.innerText);
-    title.style.color = "blue";
+function handleH1Click() {
+    h1.classList.toggle("clicked");
 }
 
-// function을 eventListener에 추가하고 event발생시 handleTitleClick 함수 실행
-title.addEventListener("click", handleTitleClick);
+h1.addEventListener("click", handleH1Click);
