53fe0031c7f2601f475a296309b487397336107a
MS <2206017@naver.com>
2022-11-25 16:14:03 +0900

    update nomadcoders

--- frontEnd/nomadCoders/vanillaJs/3.3_Events/app.js
+++ frontEnd/nomadCoders/vanillaJs/3.3_Events/app.js
@@ -1,12 +1,4 @@
-/*
-    HTML태그에 접근하는 방법은 
-    getElementById,
-    getElementsByClassName, 
-    getElementsByTagName 등이 있지만
-    querySelector을 사용하는 이유는 css처럼 접근이 가능하기때문
-
-    특정한 event 발생 시 실행할 함수를 만들고 대기시킨다.
-*/
+// html태그에 접근
 const title = document.querySelector(".hello h1");
 
 // title div를 클릭시 발생할 function 정의
@@ -15,5 +7,5 @@ function handleTitleClick () {
     title.style.color = "blue";
 }
 
-// function을 eventListener에 추가
+// function을 eventListener에 추가하고 event발생시 handleTitleClick 함수 실행
 title.addEventListener("click", handleTitleClick);
