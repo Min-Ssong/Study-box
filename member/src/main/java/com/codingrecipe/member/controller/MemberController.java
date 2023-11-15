8d44f126bd3ae7c27296a6b1f26dd7ca1edc7fa0
origin <2206017@naver.com>
2023-11-16 06:00:35 +0900

    231116 코딩레시피 회원가입_1 Day_3

--- member/src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ member/src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.codingrecipe.member.controller;
 
 import com.codingrecipe.member.dto.MemberDTO;
+import com.codingrecipe.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,7 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+    // 생성자 주입
+    private final MemberService memberService;
+
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
     public String saveForm(){
@@ -19,6 +25,7 @@ public class MemberController {
     public String save(@ModelAttribute MemberDTO memberDTO){
         System.out.println("MemberContorller.save");
         System.out.println("MemberDTO = " + memberDTO);
+        memberService.save(memberDTO);
         return "index";
     }
 
