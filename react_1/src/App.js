056c86bc7a14ccba0628e735f8f11d6ef895b4e8
origin <2206017@naver.com>
2023-12-19 06:18:53 +0900

    CodeIt "React 웹 개발 시작하기" day_3

---
+++ react_1/src/App.js
@@ -0,0 +1,13 @@
+import Dice from "./Dice";
+import HandIcon from "./HandIcon";
+
+function App(){
+    return (
+        <div>
+            <Dice />
+            <HandIcon />
+        </div>
+    );
+}
+
+export default App;
